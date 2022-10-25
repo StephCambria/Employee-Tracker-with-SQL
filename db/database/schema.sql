a91b7e380f1fc14731b56dc59295e1550621c3c6
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-25 15:00:27 -0400

    adjusting to meet criteria

--- db/database/schema.sql
+++ db/database/schema.sql
@@ -19,10 +19,17 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
+    title VARCHAR(30),
+    department VARCHAR(30),
+    salary DECIMAL,
 	manager_id INT,
-    role_id INT AUTO_INCREMENT
+    role_id INT
 );
 
 SELECT * FROM employee;
+
+
+
+
 
 
--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -15,14 +15,14 @@ VALUES
 ('Accountant', '120000', 4),
 ('Lawyer', '180000', 5);
 
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, title, department, salary, manager_id, role_id)
 VALUES 
-('Lukas', 'Horner', 1, null),
-('Saoirse', 'West', 2, 1),
-('Juno', 'Lozano', 3, 1),
-('Luis', 'Herman', 4, 2),
-('Mischa', 'Henson', 5, 2),
-('Byron', 'Riggs', 6, 1);
+('Lukas', 'Horner', 'CEO', 'CEO', '200000', null, 1),
+('Saoirse', 'West', 'Sales Lead', 'Sales', '100000', 1, 2),
+('Juno', 'Lozano', 'Lead Engineer', 'Engineering', '160000', 1, 3),
+('Luis', 'Herman', 'Web Developer', 'Engineering', '130000', 2, 4),
+('Mischa', 'Henson', 'Accountant', 'Finance', '120000', 2, 5),
+('Byron', 'Riggs', 'Lawyer', 'Legal', '180000', 1, 6);
 
 SELECT * FROM employee;
 

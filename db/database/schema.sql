ddd9e1b9309fa1b172d38feae60247afbecff9d7
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-26 15:17:51 -0400

    cleaned up database

--- db/database/schema.sql
+++ db/database/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE employee (
@@ -22,9 +22,13 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
 	manager_id INT,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
+
+
+
+
 
 
 

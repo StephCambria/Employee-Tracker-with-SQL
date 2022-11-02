372bde7c7d77e0ea332d20c3fb08852a7c8c4ec0
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-02 16:49:32 -0400

    database messy for testing purposes

--- db/database/schema.sql
+++ db/database/schema.sql
@@ -8,6 +8,8 @@ CREATE TABLE department (
     department_name VARCHAR(30)
 );
 
+-- Originally, the constraints were not in any of my tables.
+-- I added them to help with troubleshooting and bugs I encountered, and wound up keeping them.
 CREATE TABLE role (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_title VARCHAR(30),
@@ -21,7 +23,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    role_title VARCHAR(30),
+    role_title VARCHAR(30), -- I added role_title here as well for legibility within the app. 
 	manager_id INT,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
